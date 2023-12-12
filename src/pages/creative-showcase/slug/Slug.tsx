fbc0918ed377d3ad6caaa02fe92575e50c3bcac6
Robin Persson <robinpn98@hotmail.se>
2023-12-12 15:46:00 +0100

    get the slug from the url

--- src/pages/creative-showcase/slug/Slug.tsx
+++ src/pages/creative-showcase/slug/Slug.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Slug = () => {
+  //fetch the slug from the url
+  const { slug } = useParams<{ slug: string }>();
+  console.log(slug);
+
   return (
     <div>
-      <h1>Slug</h1>
+      <h1>{slug}</h1>
     </div>
   );
 };
