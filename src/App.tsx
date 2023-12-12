b3e48693920565dfd2afa3d6fb8f516901e526bf
Lucas Källberg <lucas.arne@hotmail.com>
2023-12-12 16:12:10 +0100

    working on fetching data sanity

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { client } from '../sanity.ts';
-import { Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 // pages
-import Home from './pages/home/Home.tsx';
-import Consult from './pages/consult/Consult.tsx';
-import Philanthropy from './pages/Philanthropy/Philanthropy.tsx';
-import CreativeShowcase from './pages/creative-showcase/CreativeShowcase.tsx';
-import Contact from './pages/contact/Contact.tsx';
-import PageNotFound from './pages/pageNotFound/PageNotFound.tsx';
-import Slug from './pages/Slug/slug.tsx';
+import Home from "./pages/home/Home.tsx";
+import Consult from "./pages/consult/Consult.tsx";
+import Philanthropy from "./pages/Philanthropy/Philanthropy.tsx";
+import CreativeShowcase from "./pages/creative-showcase/CreativeShowcase.tsx";
+import Contact from "./pages/contact/Contact.tsx";
+import PageNotFound from "./pages/pageNotFound/PageNotFound.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
-
-  async function fetchData() {
-    const result = await client.fetch("*[name=='Inu']");
-    console.log(result);
-  }
-
-  fetchData();
 
   return (
     <Routes>
@@ -29,7 +20,6 @@ function App() {
       <Route path="/consult" element={<Consult />} />
       <Route path="/philanthropy" element={<Philanthropy />} />
       <Route path="/creative-showcase" element={<CreativeShowcase />} />
-      <Route path="/creative-showcase/:slug" element={<Slug />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { client } from "../../../sanity.ts";
 
 const Home = () => {
+  const [pageData, setPageData] = useState({});
+
+  const fetchData = async () => {
+    const result = await client.fetch(
+      "*[_type == 'page' && title == 'Home Page'] "
+    );
+    setPageData(result);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>{pageData[0].title}</h1>
     </div>
   );
 };
